000ab4c6ac4260f1051d5287a2c5e68c8fa61cf8
Shauryakant <gshauryakant@gmail.com>
2026-01-23 15:50:04 +0530

    completed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -63,9 +63,10 @@ const userSchema = new Schema(
   },
 );
 /// hoooks (pre)
-userSchema.pre("save", async function () {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
